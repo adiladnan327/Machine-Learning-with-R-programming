adf3b5f9387833a4f1316cba427d7619d40c5119
Mohammed Adil Adnan <48308553+adiladnan327@users.noreply.github.com>
2020-01-10 02:00:12 -0500

    Add files via upload

---
+++ R programming basic.R
@@ -0,0 +1,67 @@
+deposit<- 20
+
+bank<-30
+
+bank.account<- bank + deposit
+
+
+print('R studio is nice')
+a
+
+v<-c(1,2,3)
+
+print('Frist Line')
+
+print('Second Line')
+
+#https://www.mathsisfun.com/data/standard-deviation-formulas.html
+
+#In this R we use Sample Standard deviation formaula
+#The important change is "N-1" instead of "N" (which is called "Bessel's correction").
+
+#comparision comparator
+5<6
+
+5>6
+
+5<=6
+
+5>=5
+
+2!=4
+
+2!=2
+
+2==2
+
+v <- 2
+v
+
+v< -1
+
+my.filter<-v>2
+my.filter
+
+#Logical Operator
+
+& #And
+
+x<20 & x>5
+df[(df$mpg>20) & (df$hp>100),]
+
+
+|#OR
+  
+x==100
+x==10
+(x==10)|(x==100)
+df[(df$mpg>20) | (df$hp>100),]
+
+!#Not
+
+!(10==1)
+
+
+
+
+
